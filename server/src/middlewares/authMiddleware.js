ac65c38b0fa507c1d6a00547ae29138f14ebfa6b
Davi Iacovelli <daviia05@hotmail.com.br>
2026-02-05 17:49:30 -0300

    alteração authMiddleware + .env

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -6,7 +6,21 @@ dotenv.config();
 
 // Carregando o JSON de credenciais (maneira compatível com ES Modules)
 const require = createRequire(import.meta.url);
-const serviceAccount = require("../../serviceAccountKey.json"); // Ajuste o caminho se necessário
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
+require("../../serviceAccountKey.json");
 
 // Inicializa o Firebase Admin apenas se ainda não foi inicializado
 if (!admin.apps.length) {
